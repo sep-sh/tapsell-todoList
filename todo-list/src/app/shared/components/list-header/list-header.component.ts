55dfc7c7b743dc308073143601877c3a277a357c
sep <74097138+sepsii@users.noreply.github.com>
2024-12-05 03:58:26 +0330

    fixed styles

--- todo-list/src/app/shared/components/list-header/list-header.component.ts
+++ todo-list/src/app/shared/components/list-header/list-header.component.ts
@@ -7,13 +7,14 @@ import { FormInputComponent } from "../form-input/form-input.component";
 import { TaskEventType, FormMode, ActionStatus } from '../../enums/shared.enum';
 import { ActionButtonsComponent, ActionButtonsEvent } from "../action-buttons/action-buttons.component";
 import { List, ListActionResult, ListEvent } from '../../types/shared.type';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 
 
 @Component({
   selector: 'app-list-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, FormInputComponent, ReactiveFormsModule, ActionButtonsComponent],
+  imports: [MatToolbarModule, MatTooltipModule, MatButtonModule, MatIconModule, FormInputComponent, ReactiveFormsModule, ActionButtonsComponent],
   templateUrl: './list-header.component.html',
   styleUrl: './list-header.component.scss',
   standalone: true
